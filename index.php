e7ebfdaf5a72f75d9320150cdcc29e4e0e9f2dd8
BaronNewsTechnology <44548059+BaronNewsTechnology@users.noreply.github.com>
2020-08-01 19:17:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+
+<?php 
+	include "head.html";
+?>
+
+<!-- Featured Products -->
+	<section class="" style="margin-top: 35px;">
+		<div class="container">
+			<span class="text-uppercase h4 font-weight-normal p-10">Featured Products</span>
+
+			<div class="row justify-content-start ml-1 mt-3">
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-start ml-1 mt-3">
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-start ml-1 mt-3">
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Staff pick -->
+
+	<section class="" style="margin-top: 55px;">
+		<div class="container">
+			<span class="text-uppercase h4 font-weight-normal p-10">Staff pick</span>
+
+			<div class="row justify-content-start ml-1 mt-3">
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- New Products -->
+
+	<section class="" style="margin-top: 55px;">
+		<div class="container">
+			<span class="text-uppercase h4 font-weight-normal p-10">New Products</span>
+
+			<div class="row justify-content-start ml-1 mt-3">
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-md-3 text-center mr-3 pt-3 pb-3 shadow">
+					<img class="" src="img/2.png" alt="">
+					<div class="d-flex text-left mt-4 justify-content-between">
+						<p class="w-1 m-0">Lorem, ipsum dolor. <br> <b class="text-success">$338.00</b></p>
+						<i class="fa fa-shopping-basket text-center ml-0 icons text-white" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php
+	
+	include "foot.html";
+	?>
